bc668d87b4fb8584a7c1f78c3117637aa99d3670
Subbu <ramgopalbhat10@gmail.com>
2022-11-07 21:13:46 +0530

    added friends in DB

---
+++ apps/hasura/migrations/default/1667830222481_adding_friends/down.sql
@@ -0,0 +1,5 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- INSERT into friend (name) VALUES ('Shruthi');
+-- INSERT into friend (name) VALUES ('Abhishek');
+-- INSERT into friend (name) VALUES ('Avinash');
---
+++ apps/hasura/migrations/default/1667830222481_adding_friends/up.sql
@@ -0,0 +1,3 @@
+INSERT into friend (name) VALUES ('Shruthi');
+INSERT into friend (name) VALUES ('Abhishek');
+INSERT into friend (name) VALUES ('Avinash');
